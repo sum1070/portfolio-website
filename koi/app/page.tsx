b6ccb1ceae2d62d208787b005d9e1b7d61b5af52
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-21 02:19:50 +0100

    idk

--- koi/app/page.tsx
+++ koi/app/page.tsx
@@ -1,16 +1,25 @@
 import { Hero } from "@/ui/Hero";
 import { Test } from "./test";
 import { TracingBeam } from "@/components/ui/tracing-beam";
+import WaveTransition from "@/ui/Transition";
+import MouseAnimated from "@/components/ui/MouseTrail";
 
 export default function Home() {
 
   return (
     <>
+      <MouseAnimated />
       {/* <TracingBeam className="px-1">
         <Hero />
       </TracingBeam> */}
-        {/* <Test /> */}
-        <Hero />
+      {/* <Test /> */}
+      <Hero />
+      <WaveTransition
+        waveColor="var(--color-milky-white)"
+        waveAmplitude={15}
+        speed={0.5}
+      />
+
 
     </>
 
