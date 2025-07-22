592ac0d10e397acc676ba84edc92e5335a8f5fc1
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-22 17:10:08 +0100

    Back up works with small non-breaking changes

--- koi/ui/Hero.tsx
+++ koi/ui/Hero.tsx
@@ -2,6 +2,9 @@ import BackgroundDeco from "@/components/BackgroundDeco";
 import CenterContainer from "@/components/CenterContainer";
 import { TriangleArrowDown } from "@/components/TriangleArrow";
 import Typewriter from "@/components/ui/Typewriter";
+import { StringReveal } from "@/utils/StringReveal";
+import { Reveal } from "@/utils/Reveal";
+
 
 const Text = {
   txtMain: "text-4xl sm:text-7xl xl:text-8xl text-balance tracking-tight italic font-semibold",
@@ -17,18 +20,27 @@ export const Hero = () => {
       <BackgroundDeco />
       <CenterContainer className="min-h-screen relative z-100">
         <div className="text-center text-nice-purple0 ">
+          {/* <h1 className={`${Text.txtMain}`}>
+            Lorem ipsum dolor.
+          </h1> */}
+          <Reveal>
             <h1 className={`${Text.txtMain}`}>
-              Lorem ipsum dolor.
+              Lorem ipsum dolor sit amet.
             </h1>
             <p className={`${Text.txtMid}`}>
               Lorem, ipsum dolor.
             </p>
-            <Typewriter className={`${Text.txtMono}`}>
-              Lorem ipsum dolor sit.
-            </Typewriter>
-          <div className="mt-4 pb-2 flex justify-center">
-            <TriangleArrowDown />
-          </div>
+          </Reveal>
+          <Typewriter
+            className={`${Text.txtMono}`}
+            delay={1300}
+            speed={110}
+          >
+            Lorem ipsum dolor sit.
+          </Typewriter>
+          <Reveal delay={0.5} className="mt-4 pb-2 flex justify-center">
+              <TriangleArrowDown />
+          </Reveal>
         </div>
       </CenterContainer>
     </div>
