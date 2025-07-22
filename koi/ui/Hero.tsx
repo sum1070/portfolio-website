3ed425882fc52cd54f94c6ca3d9b77a5db477213
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-22 20:58:13 +0100

    uwawa

--- koi/ui/Hero.tsx
+++ koi/ui/Hero.tsx
@@ -4,13 +4,12 @@ import BackgroundDeco from "@/components/BackgroundDeco";
 import CenterContainer from "@/components/CenterContainer";
 import { TriangleArrowDown } from "@/components/TriangleArrow";
 import Typewriter from "@/components/ui/Typewriter";
-import { StringReveal } from "@/utils/StringReveal";
 import { Reveal } from "@/utils/Reveal";
 import { useState } from "react";
 
 const Text = {
   txtMain: "text-4xl sm:text-7xl xl:text-8xl text-balance tracking-tight italic font-semibold",
-  txtMid: "mt-4 text-xl sm:text-3xl xl:text-4xl text-nice-purple0",
+  txtMid: "mt-4 text-xl sm:text-3xl xl:text-4xl",
   txtMono: "mt-4 text-base sm:text-2xl block font-titillium-web",
 };
 
@@ -18,14 +17,10 @@ const Text = {
 export const Hero = () => {
   const [revealArrow, setRevealArrow] = useState(false);
   return (
-
     <div className="pb-20">
       <BackgroundDeco />
       <CenterContainer className="min-h-screen relative z-100">
-        <div className="text-center text-nice-purple0 ">
-          {/* <h1 className={`${Text.txtMain}`}>
-            Lorem ipsum dolor.
-          </h1> */}
+        <div className="text-center text-nice-purple2 ">
           <Reveal>
             <h1 className={`${Text.txtMain}`}>
               Lorem ipsum dolor sit amet.
@@ -37,18 +32,19 @@ export const Hero = () => {
           <Typewriter
             className={`${Text.txtMono}`}
             delay={1300}
-            speed={110}
+            speed={90}
             onComplete={() => setRevealArrow(true)}
           >
             Lorem ipsum dolor sit.
           </Typewriter>
           {/* Fixed height container as placeholder */}
-          <div className="mt-6 pb-2 flex justify-center w-full h-8">
+          <div className="mt-4 pt-8 flex justify-center w-full h-8">
             {revealArrow && (
               <Reveal
                 key="arrow"
-                delay={0.1}
+                delay={0.2}
                 width="full"
+                duration={0.6}
                 className="flex justify-center w-full"
               >
                 <TriangleArrowDown />
