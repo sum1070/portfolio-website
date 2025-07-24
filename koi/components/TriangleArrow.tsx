41404f414ebf185c2f8b7b09c1857bc1f4a03f14
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-24 17:53:05 +0100

    Back up works with small non-breaking changes

--- koi/components/TriangleArrow.tsx
+++ koi/components/TriangleArrow.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import styles from "@/styles/TriangleArrow.module.css";
 
 export const TriangleArrowDown = () => {
-  return <div className={styles.scrollDown}></div>;
+  return (
+    <div
+      className="[animation:bounce_1.5s_infinite]"
+      style={{
+        width: 0,
+        height: 0,
+        margin: "0 auto",
+        borderLeft: "12px solid transparent",
+        borderRight: "12px solid transparent",
+        borderTop: "16px solid var(--color-nice-purple0)",
+      }}
+    ></div>
+  );
 };
 
 export const TriangleArrowUp = () => {
-  return <div className={styles.scrollUp}></div>;
+  return (
+    <div
+      className="[animation:bounce_2s_infinite]"
+      style={{
+        width: 0,
+        height: 0,
+        margin: "0 auto",
+        borderLeft: "12px solid transparent",
+        borderRight: "12px solid transparent",
+        borderBottom: "16px solid var(--color-nice-purple0)",
+        animation: "bounce 2s infinite",
+      }}
+    ></div>
+  );
 };
