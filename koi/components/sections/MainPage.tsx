6007d1b672ac406fc36e737a2baa7ee233c3176a
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-25 21:56:01 +0100

    [MEOW] Meow. Meow meow, meow meow meow meow. Meow.

---
+++ koi/components/sections/MainPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { CenterContainer } from '@/components'
+
+const MainPage = () => {
+    return (
+        <section className=' bg-blue-800 '>
+            <CenterContainer className="min-h-screen">
+                <div className="text-white">
+                    <h1 className="text-4xl">
+                        meow
+                    </h1>
+                    <p className="text-xl">
+                        meow
+                    </p>
+                </div>
+            </CenterContainer>
+        </section>
+    )
+}
+
+export default MainPage
