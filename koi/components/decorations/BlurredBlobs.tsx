3927779740f60e855c23660f0d199919eb8d104d
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-25 16:51:57 +0100

    [FEAT] Better blobbbbs

--- koi/components/decorations/BlurredBlobs.tsx
+++ koi/components/decorations/BlurredBlobs.tsx
@@ -3,25 +3,21 @@ import React from 'react'
 const BlurredBlobs = () => {
   return (
     <div className="absolute inset-0 overflow-hidden pointer-events-none">
+      {/* C */}
       <div
-        className="absolute -top-10 left-39 w-100 h-70 rounded-full mix-blend-multiply filter blur-xl opacity-40 [animation:blob_6s_infinite_1s_ease-in-out]"
-        style={{
-          backgroundColor: 'var(--color-pink2)',
-        }}
+        className="absolute w-100 h-100 rounded-full mix-blend-multiply filter blur-xl opacity-90 [animation:blob_6s_infinite_1s_ease-in-out] bg-linear-to-r from-sky-100 to-fuchsia-300 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       ></div>
-
+      {/* TL */}
       <div
-        className="absolute top-0 -right-4 w-96 h-96 rounded-full mix-blend-multiply filter blur-3xl opacity-55 [animation:blob2_6s_infinite_2s]"
-        style={{ backgroundColor: 'var(--color-pale-purple2)' }}
+        className='absolute rounded-full mix-blend-multiply filter blur-2xl opacity-80 [animation:blob2_6s_infinite_2s] bg-linear-60 from-[var(--color-Mauve)] to-[var(--color-pale)] top-0 right-0 w-80 h-80 '
       ></div>
-
+      {/* BR */}
       <div
-        className="absolute bottom-20 left-40 w-150 h-90 rounded-full mix-blend-multiply filter blur-2xl opacity-60 [animation:blob3_8s_infinite_2s]"
-        style={{ backgroundColor: 'var(--color-pale-purple0)' }}
+        className='absolute rounded-full mix-blend-multiply filter blur-3xl opacity-90 [animation:blob3_6s_infinite_2s] bg-linear-60 from-fuchsia-300 to-sky-300 bottom-0 right-0 w-80 h-80 lg:w-120 lg:h-120 xl:w-200 xl:h-200'
       ></div>
+      {/* BL */}
       <div
-        className="absolute bottom-20 left-50 w-100 h-90 rounded-full mix-blend-multiply filter blur-2xl opacity-60 [animation:blob2_5s_infinite_2s]"
-        style={{ backgroundColor: 'var(--color-white)' }}
+        className='absolute rounded-full mix-blend-multiply filter blur-3xl opacity-70 [animation:blob3_3s_infinite_2s] bg-linear-60 from-[var(--color-sky-blue)] to-[var(--color-pale-purple1)] bottom-0 left-0 w-60 h-60 lg:w-100 lg:h-100 xl:w-110 xl:h-110'
       ></div>
     </div>
   )
