cb3a6b4a3b8c743a102f144924cc0bcd46f9d6f8
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-07-28 14:23:06 +0100

    Back up works with small non-breaking changes

--- koi/components/sections/MainPage/BackgroundMain.tsx
+++ koi/components/sections/MainPage/BackgroundMain.tsx
@@ -12,20 +12,21 @@ export default function BackgroundHero() {
         <>
 
             <div
-                className="absolute inset-0 -z-50 "
-                style={{
-                    background: `
-                        radial-gradient(at 6% 8%, var(--color-purple0) 0%, transparent 50%),
-                        radial-gradient(at 95% 75%, var(--color-pink1) 0%, transparent 50%),
-                        radial-gradient(at 92% 27%, var(--color-sky-blue) 0%, transparent 50%),
-                        radial-gradient(at 52% 100%, var(--color-sky-blue) 0%, transparent 50%),
-                        radial-gradient(at 9% 88%, var(--color-pale-purple2) 0%, transparent 50%)
-                        `,
-                    backgroundColor: "var(--color-pale-purple0)",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundAttachment: "fixed",
-                }}
+                className="absolute inset-0 -z-50 bg-pale-purple1"
+                // style={{
+                //     background: `
+                //         radial-gradient(at 95% 75%, var(--color-pink1) 0%, transparent 50%),
+                //         radial-gradient(at 6% 48%, var(--color-purple1) 0%, transparent 80%),
+                //         radial-gradient(at 26% 5%, var(--color-milky-white) 0%, transparent 80%),
+                //         radial-gradient(at 92% 27%, var(--color-sky-blue) 0%, transparent 40%),
+                //         radial-gradient(at 52% 100%, var(--color-sky-blue) 0%, transparent 50%),
+                //         radial-gradient(at 9% 88%, var(--color-pale-purple2) 0%, transparent 20%)
+                //         `,
+                //     backgroundColor: "var(--color-pale-purple0)",
+                //     backgroundRepeat: "no-repeat",
+                //     backgroundSize: "cover",
+                //     backgroundAttachment: "fixed",
+                // }}
             >
 
 
