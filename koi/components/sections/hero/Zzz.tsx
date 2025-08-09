6cf289d513de8d87647ed69dc727931b775d0445
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-09 01:47:06 +0100

    [FEAT] add meow

---
+++ koi/components/sections/hero/Zzz.tsx
@@ -0,0 +1,16 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+import React from 'react'
+
+const Zzz = () => {
+    return (
+        <DotLottieReact
+            className="-ml-1 md:-ml-2 w-[32px] md:w-12 xl:-ml-4 xl:w-20 inline-block border-0"
+            src="https://lottie.host/0e433bf9-2680-41ec-88d5-996562f0e561/zIA3MnTsh5.lottie"
+            loop
+            autoplay
+        />
+    )
+}
+
+export default Zzz
