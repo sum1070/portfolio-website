8112e7a49e48866e77eddc01a95952e51713ea64
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-12 19:37:02 +0100

    [FIX] Update icon in licence page

--- koi/app/licences/SvgLicence.ts
+++ koi/app/licences/SvgLicence.ts
@@ -5,19 +5,19 @@ const svgLicenceLinks: CardDetails[] = [
   {
     name: "Creative Commons Attribution SVG Vector - geakstr",
     url: "https://www.svgrepo.com/svg/353062/creative-commons-attribution",
-    icon: iconImages.attr4,
+    icon: "attr4",
     iconAlt: "Attribution 4.0",
   },
   {
     name: "Medal Star SVG Vector - Iconsax",
     url: "https://www.svgrepo.com/svg/495465/medal-star",
-    icon: iconImages.medal,
+    icon: "medal",
     iconAlt: "medal",
   },
   {
     name: "Discord SVG Vector - primefaces",
     url: "https://www.svgrepo.com/svg/506463/discord",
-    icon: iconImages.medal,
+    icon: "medal",
     iconAlt: "Discord Icon",
   }
 ];
