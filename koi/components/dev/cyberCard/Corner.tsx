87c6f82e7e82cd41e569ef4094473b0ec9325cdb
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-13 20:38:38 +0100

    Back up works with small non-breaking changes

--- koi/components/dev/cyberCard/Corner.tsx
+++ koi/components/dev/cyberCard/Corner.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import { cn } from '@/utils';
 
-const CyberCardCorner = ({ isHovered = false }) => {
+interface CornerProps {
+  isHovered?: boolean;
+}
+
+const Corner = ({ isHovered = false }: CornerProps) => {
   const corner = "absolute transition-all duration-300 border-indigo-500/20";
-  const hoveredStyle = "w-[20px] h-[20px] border-cyan-400/50";
-  const positionValue = "10px";
-  const sizeValue = "15px";
-  const pos = {
-    topLeft: `top-[${positionValue}] left-[${positionValue}] w-[${sizeValue}] h-[${sizeValue}] border-t-2 border-l-2`,
-    topRight: `top-[${positionValue}] right-[${positionValue}] w-[${sizeValue}] h-[${sizeValue}] border-t-2 border-r-2`,
-    bottomLeft: `bottom-[${positionValue}] left-[${positionValue}] w-[${sizeValue}] h-[${sizeValue}] border-b-2 border-l-2`,
-    bottomRight: `bottom-[${positionValue}] right-[${positionValue}] w-[${sizeValue}] h-[${sizeValue}] border-b-2 border-r-2`,
-  };
+  const hoveredStyle = "w-[20px] h-[20px] border-[rgba(0,242,234,0.5)] ";
 
   return (
     <div className="corner-elements pointer-events-none">
       <span className={cn(
-        corner, pos.topLeft,
-        isHovered && hoveredStyle
-      )}></span>
-      <span className={cn(
-        corner, pos.topRight,
-        isHovered && hoveredStyle
-      )}></span>
-      <span className={cn(
+        "top-[10px] left-[10px] w-[15px] h-[15px] border-t-2 border-l-2 ",
         corner,
-        pos.bottomLeft,
         isHovered && hoveredStyle
       )}></span>
       <span className={cn(
+        "top-[10px] right-[10px] w-[15px] h-[15px] border-t-2 border-r-2 ",
         corner,
-        pos.bottomRight,
+        isHovered && hoveredStyle
+      )}></span>
+      <span className={cn(
+        "bottom-[10px] left-[10px] w-[15px] h-[15px] border-b-2 border-l-2 ",
+        corner,
+        isHovered && hoveredStyle
+      )}></span>
+      <span className={cn(
+        "bottom-[10px] right-[10px] w-[15px] h-[15px] border-b-2 border-r-2 ",
+        corner,
         isHovered && hoveredStyle
       )}></span>
     </div>
   );
 };
-export default CyberCardCorner;
+
+export default Corner;
