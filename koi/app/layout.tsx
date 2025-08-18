300c2de7d42baebb8861f5028f716e442c98798e
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-18 15:31:55 +0100

    [FIX] fix loading animation

--- koi/app/layout.tsx
+++ koi/app/layout.tsx
@@ -27,29 +27,27 @@ export default function RootLayout({
           {children}
           <SpeedInsights />
           <Analytics />
-          <div id="page-transition-overlay">
-            <div className="min-w-screen min-h-screen flex items-center justify-center relative">
-              {bgPrimary()}
-              <div className="flex flex-col items-center justify-center gap-8">
-                <div className="min-h-[180px] flex flex-col items-center justify-center gap-4">
-                  <span className="eyes"></span>
-                  <span className="spinner"></span>
-                </div>
-                <div>
-                  <div
-                    className={cn(
-                      "loading loading04",
-                      "text-4xl lg:text-8xl font-extrabold tracking-wider",
-                    )}
-                  >
-                    <span>L</span>
-                    <span>O</span>
-                    <span>A</span>
-                    <span>D</span>
-                    <span>I</span>
-                    <span>N</span>
-                    <span>G</span>
-                  </div>
+          <div id="page-transition-overlay" className="min-w-screen min-h-screen flex items-center justify-center relative">
+            {bgPrimary()}
+            <div className="flex flex-col items-center justify-center gap-8">
+              <div className="min-h-[180px] flex flex-col items-center justify-center gap-4">
+                <span className="eyes"></span>
+                <span className="spinner"></span>
+              </div>
+              <div>
+                <div
+                  className={cn(
+                    "loading loading04",
+                    "text-4xl lg:text-8xl font-extrabold tracking-wider",
+                  )}
+                >
+                  <span>L</span>
+                  <span>O</span>
+                  <span>A</span>
+                  <span>D</span>
+                  <span>I</span>
+                  <span>N</span>
+                  <span>G</span>
                 </div>
               </div>
             </div>
