7310b1137348cb326f2189ceec080f74a5af221c
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-24 02:32:59 +0100

    [FEAT] lsfdfkdsa

--- koi/app/contact/page.tsx
+++ koi/app/contact/page.tsx
@@ -18,15 +18,14 @@ const Contact = () => {
       <div className="w-full pt-16 pb-40 px-8 sm:px-12 md:px-24 max-w-6xl mx-auto">
         <div id='contact-text' className="md:mb-12 sm:mb-8 mb-4">
           <h1 className={cn(
-            "font-black text-center",
+            "font-black text-center ",
           )}>
             <Watching
               className="w-[16svw] md:w-[10svw] inline-block ml-2 object-contain"
-              // darkModeColor="#7dd3fc" // light sky-300 color for dark mode
             />
           </h1>
           <h2 className={cn(
-            "mb-1 text-center",
+            "mb-1 text-center dark:text-[#7dd3fc]",
           )}>
             You wanna find me, huh?
           </h2>
