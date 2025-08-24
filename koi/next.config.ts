55e0a3227756c618c1ee8132ee07094ded36af1e
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-24 03:38:03 +0100

    [FEAT] dark mode

--- koi/next.config.ts
+++ koi/next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    staleTimes: {
-      dynamic: 30,
-      static: 180,
-    },
-  },
 };
 
 export default nextConfig;
