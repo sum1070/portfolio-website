c2c83af719317fc4934d452cc826d0770c39c63d
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2025-08-24 19:12:36 +0100

    [FEAT] quicker animation

--- koi/utils/motion-utils.ts
+++ koi/utils/motion-utils.ts
@@ -2,15 +2,15 @@
 export const animationTime = {
   delayOrbitIntro: 0,
   durationOrbitIntro: 0.3,
-  delayOrbitColor: 1,
-  durationOrbitColor: 1,
-  delayBG: 0.6,
-  durationBG: 0.4,
+  delayOrbitColor: 0.6,
+  durationOrbitColor: 0.8,
+  delayBG: 0.45,
+  durationBG: 0.3,
   durationBars: 1,
   durationDots: 0.8,
-  delayMainTxt: 0.5,
+  delayMainTxt: 0.4,
   durationMainTxt: 0.2,
-  delayTriangleArrow: 0.5,
+  delayTriangleArrow: 0.4,
   durationTriangleArrow: 0.4,
   delayTypewriter: 200, // 3 digits (ms)
   durationTypewriter: 500, // 3 digits (ms)
