bf6d7b40461554738c18afaa4a15145a0cb8462c
Kit Sum Chan <139811822+sum1070@users.noreply.github.com>
2026-09-20 01:25:13 +0100

    [FEAT] Add browser startup page project

---
+++ koi/data/projects/browser-startup.ts
@@ -0,0 +1,18 @@
+import { TProject } from "@/lib/types";
+
+const startUpPage: TProject = {
+  slug: "startup-page",
+  visible: true,
+  title: "Custom Browser Startup Page",
+  shortDescription:
+    "A customisable browser start page with bookmark groups, flexible layouts, and notes, saved locally in your browser.",
+  tags: ["WebDev"],
+  technologies: ["React", "TypeScript", "Vite", "Tailwind CSS"],
+  previewImg: [],
+  github: undefined,
+  demo: "https://my-startpage-mu.vercel.app/",
+  linkTitle: true,
+  lastUpdate: undefined,
+};
+
+export default startUpPage;
